bbdde3eae29d2dd778f605cb2e28bb71fe258f43
john murphy <jaxkmurphy@gmail.com>
2024-02-22 21:20:57 +0000

    Messing with c++

---
+++ Source/Ascend/GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AtestingMigrateGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AtestingMigrateGameMode();
+};
+
+
+
